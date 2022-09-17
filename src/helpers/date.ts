f5bbf5d39043488d51d5f5bf7fa08fed21c4ffdc
carlos <carlos.gonzalez@360medics.com>
2022-09-18 00:03:12 +0200

    chore(helpers): add jsdoc on formatdate function

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -2,6 +2,13 @@ import { format } from 'date-fns';
 
 const DATE_FORMAT = 'yyyy-MM-dd';
 
+/**
+ * formatDate is a function used to convert date
+ * @example formatDate('Sun Sep 12 1999 23:15:46 GMT+0200 (Central European Summer Time)');
+ *  // returns '1999-09-12'
+ * @param {string} date
+ * @returns {string}  Returns date with format yyyy-MM-dd
+ */
 const formatDate = (date: Date): string => {
     return format(date, DATE_FORMAT);
 };
