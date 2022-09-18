5c16569875475b340ec914e81672d765a992f33a
carlos <carlos.gonzalez@360medics.com>
2022-09-19 01:19:00 +0200

    refactor(app): change the tree structure

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,23 +1,38 @@
 import React, { useState } from 'react';
-import { formatDate } from '../../helpers/date';
-import { DatePicker } from '../DatePicker';
+// ? Helpers
+import { formatDate, yesterday } from '../../helpers/date';
 import { getNasaApodUrl } from '../../helpers/url';
+// ? Hooks
 import useFetch from '../../hooks/useFetch';
+// ? Components
+import { Card } from '../Card';
+import { Container } from '../Container';
+import { DatePicker } from '../DatePicker';
+import { Error } from '../Error';
+import { Loader } from '../Loader';
+import { Hero } from '../Hero';
+import { Main } from '../Main';
 
 function App() {
-    const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+    const [selectedDate, setSelectedDate] = useState<Date>(yesterday);
     const url = getNasaApodUrl(formatDate(selectedDate));
-    const [data, isLoading, isError] = useFetch(url);
-    console.log(data);
+    const [data, isLoading, isError, errorMsg] = useFetch(url);
 
-    if (isError) return <p>There is an error.</p>;
-    if (isLoading) return <p>Loading...</p>;
+    if (isLoading) return <Loader />;
     return (
-        <div className="App">
-            <header className="App-header"> Hello </header>
-
-            <DatePicker setSelectedDate={setSelectedDate} selectedDate={selectedDate} />
-        </div>
+        <Main>
+            <Hero>
+                <Container>
+                    <DatePicker setSelectedDate={setSelectedDate} selectedDate={selectedDate} />
+                    {isError && <Error message={`${errorMsg}`} />}
+                </Container>
+                {data && (
+                    <Container>
+                        <Card data={data} />
+                    </Container>
+                )}
+            </Hero>
+        </Main>
     );
 }
 
