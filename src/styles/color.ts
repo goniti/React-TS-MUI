8d88faaec9231ab6fe9c6ae2958fdaec38979deb
carlos <carlos.gonzalez@360medics.com>
2022-09-19 01:12:37 +0200

    style(variable): add vars and color

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,3 +1,8 @@
 export const color: { [key: string]: string } = {
-    primary: '#383',
+    primary: '#39c8bf',
+    secondary: '#566576',
+    tertary: '#b7b7b7',
+    dark: '#1a1a1a',
+    light: '#faffff',
+    danger: '#ea4c89',
 };
---
+++ src/styles/vars.ts
@@ -0,0 +1,6 @@
+export const vars: { [key: string]: string } = {
+    gutter: '.3rem',
+    bigGutter: '5rem',
+    iconWrapperSize: '3.5rem',
+    iconSize: '2rem',
+};
